c107bc12c11fc50f514f4f8b15d058145edcac44
lemoncloud <lemoncloud@gmail.com>
2023-08-28 17:24:58 +0900

    buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { styled } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const MyButton = styled(Button)({
-  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-  border: 0,
-  borderRadius: 3,
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
-  height: 48,
-  padding: '0 30px',
-});
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
+export default function ContainedButtons() {
+  const classes = useStyles();
 
-export default function StyledComponents() {
-  return <MyButton>Styled Components</MyButton>;
+  return (
+    <div className={classes.root}>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+    </div>
+  );
 }
