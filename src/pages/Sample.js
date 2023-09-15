212f402091cea2a8cde9164913d9bb61f6ca092d
lbjjang <54067542+lbjjang@users.noreply.github.com>
2023-09-15 18:03:10 +0900

    ㅇㅇㅇ
    ㅇㅇㅇ

--- src/pages/Sample.js
+++ src/pages/Sample.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Box, Paper, Stack } from "@mui/material";
 
 import Buttons from "../components/Buttons";
@@ -7,7 +7,43 @@ import { BsPencilSquare, BsEye } from "react-icons/bs";
 import { PiArrowCounterClockwiseBold } from "react-icons/pi";
 import { IoDocumentOutline } from "react-icons/io5";
 
+//팝업 사용하는 애들
+import Button from '@mui/material/Button';
+import DialogPop from '../components/DialogPop';
+import Typography from "@mui/material/Typography";
+import DialogActions from '@mui/material/DialogActions';
+
+var pop01;
+var pop02;
+var pop03;
+
+
 export default function Sample() {
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDialogName, setDialogName] = useState('');
+  
+  const openDialog = (pop) => {
+    setIsDialogOpen(true);
+    //클릭한 타겟의 name 가져오기
+   
+    //클릭한 타겟의 pop 구분 가져오기
+    if(pop == 'pop01'){
+      pop01 = true;
+      pop02 = false;
+      pop03 = false;
+    } else if(pop == 'pop02'){
+      pop01 = false;
+      pop02 = true;
+      pop03 = false;
+    } else if(pop == 'pop03'){
+      pop01 = false;
+      pop02 = false;
+      pop03 = true;
+    }
+  }
+  const closeDialog = () => setIsDialogOpen(false);
+
   return (
     <>
       <Stack direction="row">
@@ -61,8 +97,71 @@ export default function Sample() {
               엑셀
             </Buttons>
           </Stack>
+          <Box>
+          <Button onClick={() => openDialog('pop01')}>
+            팝업1
+          </Button>
+          <Button onClick={() =>openDialog('pop02')}>
+            팝업2
+          </Button>
+          <Button onClick={() =>openDialog('pop03')}>
+            팝업3
+          </Button>
+          </Box>
         </Box>
       </Stack>
+
+
+
+      <DialogPop
+        isOpen={isDialogOpen}
+        closeDialog={closeDialog}
+        title={'타이틀 영역'}
+      >
+      {pop01 && 
+        <Box>
+          <Typography variant="h6" component="h2">
+            안녕하세요
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            컨텐츠 1 영역입니다.
+          </Typography>
+          <DialogActions>
+            <Button onClick={closeDialog}>1번 버튼</Button>
+            <Button onClick={closeDialog} autoFocus>1번 버튼이다</Button>
+          </DialogActions>        
+        </Box>
+      }
+      {pop02 && 
+        <Box>
+          <Typography variant="h6" component="h2">
+            안녕하세요
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            컨텐츠 2 영역입니다.
+          </Typography>
+          <DialogActions>
+            <Button onClick={closeDialog}>2번 버튼</Button>
+            <Button onClick={closeDialog} autoFocus>2번 버튼이다</Button>
+          </DialogActions>                
+        </Box>
+      }
+      {pop03 && 
+        <Box>
+          <Typography variant="h6" component="h2">
+            안녕하세요
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            컨텐츠 3 영역입니다.
+          </Typography>
+          <DialogActions>
+            <Button onClick={closeDialog}>3번 버튼</Button>
+            <Button onClick={closeDialog} autoFocus>3번 버튼이다</Button>
+          </DialogActions>    
+        </Box>
+      }
+      </DialogPop>
+
     </>
   );
 }
